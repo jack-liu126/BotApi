10b223d83954fe94e8cb6c603ea499e868d01c7a
Jack2 Liu <Jack2_Liu@asus.com>
2023-10-20 14:43:59 +0800

    加入專案檔案。

---
+++ Controllers/BotController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public async Task<IActionResult> HelloWorld()
+        {
+            ContentResult cr = new ContentResult();
+            return cr;
+        }
+    }
+}
